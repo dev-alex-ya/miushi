c63a561a03fa79a252e6a753d7534871e4b36a24
Alex <ya.alexandr.86@gmail.com>
2019-02-09 14:25:12 +0200

    seet property of slides

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-$(document).ready(function(){
-  
-	// Toggler collapse
-  // $("button.toggler").click(function(){
-  //   $(".toggler + .collapse").toggle();
-  // });
+// require("slick-carousel");
+$(document).ready(function() {
 
+    $(".slider").slick({
 
+        // normal options...
+        slidesToShow: 4,
+        infinite: true,
+        focusOnSelect: true,
+        centerMode: true,
+        centerPadding: "35px",
+        mobileFirst: true,
+
+        // the magic
+        responsive: [
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    infinite: true
+                }
+            }
+            
+        ]
+    });
+
+    // $('.slider').slick();
 });
